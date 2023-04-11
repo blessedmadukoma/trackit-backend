e14cb7dfedcd5ae849043f6ec474ba925347df21
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-11 01:48:15 +0100

    fixed?

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ func main() {
 	// 	log.Fatal("Cannot load config:", err)
 	// }
 	config := util.LoadEnvConfig(".")
-	fmt.Println(config)
+	// fmt.Println(config)
+
 	// connect to database
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
